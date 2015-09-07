3bf1b27c1dc18fa8523195b14fc58b7cd3d8861d
Max Chadwick <mpchadwick@gmail.com>
2015-09-07 11:33:55 -0400

    Remove unused method

--- app/code/community/SomethingDigital/Modal/Helper/Data.php
+++ app/code/community/SomethingDigital/Modal/Helper/Data.php
@@ -2,14 +2,4 @@
 
 class SomethingDigital_Modal_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    protected $_blockId;
-
-    public function getBlockId()
-    {
-        if (!$this->_blockId) {
-            $this->_blockId = Mage::getStoreConfig('cms/modal/block');
-        }
-        return $this->_blockId;
-    }
-
 }
